682547343bde11983bda8acc3c14f91b86053a2e
pavedom13 <pavedom13@gmail.com>
2020-12-09 13:36:40 +0300

    адаптив header+burger

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-
-$('.slider').slick({
-    arrows: false,//убираем стрелки
-    slidesToShow: 3,//колличество слайдлов в окне
-    slidesToScroll: 3,//пролистывается по 3 слайда
-    autoplay: true,//автоматическое перелистывание
-    autoplaySpeed: 2000,//скорость перелистывания
-    dots: true,//добавляем дотс
-  });
-
-
-  
+$(function () {
+    //слайдер и его настройки
+    $('.slider').slick({
+        arrows: false,//убираем стрелки
+        slidesToShow: 3,//колличество слайдлов в окне
+        slidesToScroll: 3,//пролистывается по 3 слайда
+        autoplay: true,//автоматическое перелистывание
+        autoplaySpeed: 2000,//скорость перелистывания
+        dots: true,//добавляем дотс
+    });
+    //находим бургер. при клике выходит меню при вторм клике прячется обратно
+    $('.header__burger').on('click', function () {
+        //при клике на бургер появляется новый класс- menu__list-active
+        $('.menu__list').toggleClass('menu__list-active')
+    });
+});
