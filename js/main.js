cf0897c91106db4f7d42d57c8119209bba721fb7
pavedom13 <pavedom13@gmail.com>
2020-12-10 09:49:26 +0300

    исправлены заголовки.высота строки.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function () {
         arrows: false,//убираем стрелки
         slidesToShow: 3,//колличество слайдлов в окне
         slidesToScroll: 3,//пролистывается по 3 слайда
-        //autoplay: true,//автоматическое перелистывание
+        autoplay: true,//автоматическое перелистывание
         autoplaySpeed: 2000,//скорость перелистывания
         dots: true,//добавляем дотс
         responsive: [
